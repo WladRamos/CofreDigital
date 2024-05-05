cf9b6ea3c77563f93c3c469bcdc38f5c5edec120
mschuler10 <nina_schuler@hotmail.com>
2024-05-05 11:31:09 -0300

    Update AutenticaUsuario.java

--- CofreDigital/src/AutenticaUsuario.java
+++ CofreDigital/src/AutenticaUsuario.java
@@ -1,1 +1,18 @@
- 
+ public class AutenticaUsuario {
+
+    public static String[] usuarioExiste(String email){
+        // Se usuário existe no banco, retorna as informações do usuário
+        String[] usuario = {"nome", "grupo", "acessos"};
+        return usuario;
+        // Se usuário não existir, retorna NULL
+    }
+
+    public static boolean verificaSenha(String senha){
+        return true;
+    }
+
+    public static boolean verificaTOTP(String totp){
+        return true;
+    }
+
+ }
